eff6f7beaeb2cf434e197d874341a6d7096e5406
swathidhanu <81679244+Swathi0123@users.noreply.github.com>
2023-07-21 14:58:10 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,6 +8,20 @@ terraform {
  }
 }
 
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+
+terraform {
+  backend "s3" {
+    bucket         = "poceks1"
+    key            = "terraform.tfstate"
+    region         = "ap-southeast-1"
+    dynamodb_table = "sample"
+  }
+}
+
 provider "kubernetes" {
  config_path = "/root/.kube/config"
 }
